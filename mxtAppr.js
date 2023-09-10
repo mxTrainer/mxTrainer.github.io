3305b8cdf36d0843f42a6fa0b0044c300261c8d8
mxTrainer <144573030+mxTrainer@users.noreply.github.com>
2023-09-09 23:06:30 -0500

    Update mxtAppr.js

--- mxtAppr.js
+++ mxtAppr.js
@@ -54,6 +54,11 @@ function isTrainSame(train1, train2){ //assumes trains are of the same length. V
   return true;
 }
 
+let halfWidth = window.innerWidth/2
+c.onclick = function(e){
+    if(e.pageX <= halfWidth && displayed != 0){finished = false; displayTrain(finalArray[0][--displayed]); index.textContent = "#" + (displayed + 1) + "/" + numTrains; finished = true;}else if(displayed != finalArray[0].length - 1){finished = false; displayTrain(finalArray[0][++displayed]); index.textContent = "#" + (displayed + 1) + "/" + numTrains; finished = true;}
+}
+
 let displayed = 0;
 let index = document.getElementById("displayed");
 window.addEventListener("keydown",function(e){
