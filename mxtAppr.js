f52d395a2f12ed0a43042100dba518fa206698fa
mxTrainer <144573030+mxTrainer@users.noreply.github.com>
2023-09-09 22:26:27 -0500

    -

---
+++ mxtAppr.js
@@ -0,0 +1,64 @@
+let finished = true;
+
+function appraiseSort(){
+  for(let i = 0; i < finalArray[0].length; i++){
+    finalArray[0][i].appr = trainValue(finalArray[0][i]);
+    sortTrain(finalArray[0][i]);
+  }
+}
+
+function trainValue(train){ //also searches for double blank (db)
+  let total = 0;
+  for(let count = 0; count < train.length; count++){
+    let db = total;
+    total += train[count][0] + train[count][1];
+    if(db == total){total += 50}
+  }
+  return total;
+}
+
+function mxtSort(type){
+  finalArray[0].sort(function (a,b) {
+    if(type == 0){//sort by length sub appr
+      if(a.length != b.length){return b.length - a.length}
+      else{return b.appr - a.appr}
+    }else if(type == 1){//sort by appr sub length
+      if(a.appr != b.appr){return b.appr - a.appr}
+      else{return b.length - a.length}
+    }
+
+});
+}
+
+function userTrain(u_train){
+  let fa_len = finalArray[0].length;
+  let user_len = u_train.length;
+  u_train.appr = trainValue(u_train);
+
+  for(let i = 0; i < fa_len; i++){
+    if(finalArray[0][i].length == user_len && finalArray[0][i].appr == u_train.appr){
+      if(isTrainSame(finalArray[0][i], u_train)){displayTrain(finalArray[0][i]); displayed = i; index.textContent = "#" + (i + 1) + "/" + fa_len; return;}
+    }
+  }
+  alert("Error: Train not found.")
+}
+
+function isDominoSame(d1, d2){
+  if( d1[0] == d2[0] && d1[1] == d2[1]){return true;}
+  else{return false;}
+}
+function isTrainSame(train1, train2){ //assumes trains are of the same length. Verify before calling.
+  for(let i = 0; i < train1.length; i++){
+    if( !isDominoSame(train1[i], train2[i]) ){return false}
+  }
+  return true;
+}
+
+let displayed = 0;
+let index = document.getElementById("displayed");
+window.addEventListener("keydown",function(e){
+    if(finished == true){
+      if(e.keyCode == 37 && displayed != 0){finished = false; displayTrain(finalArray[0][--displayed]); index.textContent = "#" + (displayed + 1) + "/" + numTrains; finished = true;}
+      else if(e.keyCode == 39 && displayed != finalArray[0].length - 1){finished = false; displayTrain(finalArray[0][++displayed]); index.textContent = "#" + (displayed + 1) + "/" + numTrains; finished = true;}
+    }
+})
---
+++ mxtCanvas.js
@@ -0,0 +1,173 @@
+const c = document.getElementById("c");
+const ctx = c.getContext('2d');
+ctx.translate(0.5, 0.5);
+c.width = window.innerWidth - 60;
+c.height = c.width;
+
+const rowLimit = 5;
+const dW = c.width/rowLimit;
+const dH = dW/2;
+const spacer = dH/4;
+
+let dotW = 10;
+
+let bPrint = [
+  [],
+  [0,0,0,0,1,0,0,0,0],
+  [1,0,0,0,0,0,0,0,1],
+  [1,0,0,0,1,0,0,0,1],
+  [1,0,1,0,0,0,1,0,1],
+  [1,0,1,0,1,0,1,0,1],
+  [1,1,1,0,0,0,1,1,1],
+  [1,1,1,0,1,0,1,1,1],
+  [1,1,1,1,0,1,1,1,1],
+  [1,1,1,1,1,1,1,1,1]];
+
+//let bPrintColor = ["","lightblue","green","pink","darkgrey","darkblue","gold","blue","darkgreen","purple","orange","black","grey"]
+let bPrintColor = ["","#00B6FF","green","#FF008E","darkgrey","darkblue","#FFEB23","#87A1BE","darkgreen","#5C0B94","orange","black","#B7B7B7"]
+
+function displayTrain(train){
+  //let w = 100; //window.innerWidth/rowLimit;
+  //let h = w/2;
+  cls(); //clear old train
+  for(let i = 0; i < train.length; i++){
+    let x = dW*(i%rowLimit);
+    let y = (dH+spacer)*(Math.floor(i/rowLimit));
+    drawDomino(x,y,dW,dH,10,train[i]);
+  }
+}
+
+function drawDomino(x,y,w,h,r,d){
+  const xW = x+w; //used three times in the function,
+  const yH = y+h; //saved instead of calcualting it over and over again.
+
+  ctx.lineWidth = 4;
+
+  ctx.beginPath();
+  ctx.moveTo(x+r, y); //starting point
+  ctx.arcTo(xW, y,  xW,   y+r,  r); //top
+  ctx.arcTo(xW, yH, xW-r, yH,   r); //right
+  ctx.arcTo(x,  yH, x,    yH-r, r); //bottom
+  ctx.arcTo(x,  y,  x+r,  y,    r); //left
+  ctx.fillStyle = "white";
+  ctx.strokeStyle = "black"
+  ctx.fill();
+  ctx.stroke();
+
+  //ctx.save();
+
+  ctx.beginPath();
+  ctx.moveTo(x + w/2, y + h * 0.1);
+  ctx.lineTo(x + w/2, yH - h * 0.1 ); //center line
+  ctx.stroke();
+  //ctx.save();
+
+  for(let j = 0; j < 2; j++){
+    if(d[j] > 9){twelvePattern(x, y, d[j], j);}
+    else if(d[j] > 0){ninePattern(x, y, d[j], j);}
+  }
+
+  //ctx.restore();
+}
+
+function ninePattern(x,y,n,pos){
+  side = pos*dW/2;
+  halfDW = dW / 2;
+  spaceX = (halfDW - dotW*3)/4;
+  spaceY = (dH - dotW*3)/4;
+  spaceDotX = dotW + spaceX;
+  spaceDotY = dotW + spaceY;
+  ctx.beginPath();
+  ctx.fillStyle = bPrintColor[n];
+
+  if(bPrint[n][0]){
+    ctx.moveTo(x + spaceX + dotW/2 + side, y + spaceY + dotW/2);
+    ctx.arc(x + spaceX + dotW/2 + side, y + spaceY + dotW/2, dotW, 0, 2 * Math.PI);
+  }
+  if(bPrint[n][1]){
+    ctx.moveTo(x + spaceDotX + spaceX + dotW/2 + side, y + spaceY + dotW/2);
+    ctx.arc(x + spaceDotX + spaceX + dotW/2 + side, y + spaceY + dotW/2, dotW, 0, 2 * Math.PI);
+  }
+  if(bPrint[n][2]){
+    ctx.moveTo(x + (spaceDotX * 2) + spaceX + dotW/2 + side, y + spaceY + dotW/2);
+    ctx.arc(x + (spaceDotX * 2) + spaceX + dotW/2 + side, y + spaceY + dotW/2, dotW, 0, 2 * Math.PI);
+  }
+  //row 2
+  if(bPrint[n][3]){
+    ctx.moveTo(x + spaceX + dotW/2 + side, y + spaceY + dotW/2 + (spaceDotY * 2));
+    ctx.arc(x + spaceX + dotW/2 + side, y + spaceY + dotW/2 + spaceDotY, dotW, 0, 2 * Math.PI);
+  }
+  if(bPrint[n][4]){
+    ctx.moveTo(x + spaceDotX + spaceX + dotW/2 + side, y + spaceY + dotW/2 + spaceDotY);
+    ctx.arc(x + spaceDotX + spaceX + dotW/2 + side, y + spaceY + dotW/2 + spaceDotY, dotW, 0, 2 * Math.PI);
+  }
+  if(bPrint[n][5]){
+    ctx.moveTo(x + (spaceDotX * 2) + spaceX + dotW/2 + side, y + spaceY + dotW/2 + spaceDotY);
+    ctx.arc(x + (spaceDotX * 2) + spaceX + dotW/2 + side, y + spaceY + dotW/2 + spaceDotY, dotW, 0, 2 * Math.PI);
+  }
+//row 3
+  if(bPrint[n][6]){
+    ctx.moveTo(x + spaceX + dotW/2 + side, y + spaceY + dotW/2 + (spaceDotY * 2));
+    ctx.arc(x + spaceX + dotW/2 + side, y + spaceY + dotW/2 + (spaceDotY * 2), dotW, 0, 2 * Math.PI);
+  }
+  if(bPrint[n][7]){
+    ctx.moveTo(x + spaceDotX + spaceX + dotW/2 + side, y + spaceY + dotW/2 + (spaceDotY * 2));
+    ctx.arc(x + spaceDotX + spaceX + dotW/2 + side, y + spaceY + dotW/2 + (spaceDotY * 2), dotW, 0, 2 * Math.PI);
+  }
+  if(bPrint[n][8]){
+    ctx.moveTo(x + (spaceDotX * 2) + spaceX + dotW/2 + side, y + spaceY + dotW/2 + (spaceDotY * 2));
+    ctx.arc(x + (spaceDotX * 2) + spaceX + dotW/2 + side, y + spaceY + dotW/2 + (spaceDotY * 2), dotW, 0, 2 * Math.PI);
+  }
+
+  ctx.fill();
+  //ctx.save();
+}
+function twelvePattern(x,y,n,pos){
+  side = pos*dW/2;
+  halfDW = dW / 2;
+  spaceX = (halfDW - dotW*4)/5;
+  spaceY = (dH - dotW*3)/4;
+  spaceDotX = dotW + spaceX;
+  spaceDotY = dotW + spaceY;
+  ctx.beginPath();
+  ctx.fillStyle = bPrintColor[n];
+    ctx.moveTo(x + spaceX + dotW/2 + side, y + spaceY + dotW/2);
+  ctx.arc(x + spaceX + dotW/2 + side, y + spaceY + dotW/2, dotW, 0, 2 * Math.PI);
+    ctx.moveTo(x + spaceDotX + spaceX + dotW/2 + side, y + spaceY + dotW/2);
+  ctx.arc(x + spaceDotX + spaceX + dotW/2 + side, y + spaceY + dotW/2, dotW, 0, 2 * Math.PI);
+      ctx.moveTo(x + (spaceDotX * 2) + spaceX + dotW/2 + side, y + spaceY + dotW/2);
+  ctx.arc(x + (spaceDotX * 2) + spaceX + dotW/2 + side, y + spaceY + dotW/2, dotW, 0, 2 * Math.PI);
+      ctx.moveTo(x + (spaceDotX * 3) + spaceX + dotW/2 + side, y + spaceY + dotW/2);
+  ctx.arc(x + (spaceDotX * 3) + spaceX + dotW/2 + side, y + spaceY + dotW/2, dotW, 0, 2 * Math.PI);
+    //row 2
+    ctx.moveTo(x + spaceX + dotW/2 + side, y + spaceY + dotW/2 + (spaceDotY * 2));
+  ctx.arc(x + spaceX + dotW/2 + side, y + spaceY + dotW/2 + spaceDotY, dotW, 0, 2 * Math.PI);
+  if(n == 12){
+      ctx.moveTo(x + spaceDotX + spaceX + dotW/2 + side, y + spaceY + dotW/2 + spaceDotY);
+    ctx.arc(x + spaceDotX + spaceX + dotW/2 + side, y + spaceY + dotW/2 + spaceDotY, dotW, 0, 2 * Math.PI);
+        ctx.moveTo(x + (spaceDotX * 2) + spaceX + dotW/2 + side, y + spaceY + dotW/2 + spaceDotY);
+    ctx.arc(x + (spaceDotX * 2) + spaceX + dotW/2 + side, y + spaceY + dotW/2 + spaceDotY, dotW, 0, 2 * Math.PI);
+  }
+  else if(n == 11){
+        ctx.moveTo(x + halfDW/2 + side, y + spaceY + dotW/2 + spaceDotY);
+    ctx.arc(x + halfDW/2 + side, y + spaceY + dotW/2 + spaceDotY, dotW, 0, 2 * Math.PI);
+  }
+      ctx.moveTo(x + (spaceDotX * 3) + spaceX + dotW/2 + side, y + spaceY + dotW/2 + spaceDotY);
+  ctx.arc(x + (spaceDotX * 3) + spaceX + dotW/2 + side, y + spaceY + dotW/2 + spaceDotY, dotW, 0, 2 * Math.PI);
+  //row 3
+    ctx.moveTo(x + spaceX + dotW/2 + side, y + spaceY + dotW/2 + (spaceDotY * 2));
+  ctx.arc(x + spaceX + dotW/2 + side, y + spaceY + dotW/2 + (spaceDotY * 2), dotW, 0, 2 * Math.PI);
+    ctx.moveTo(x + spaceDotX + spaceX + dotW/2 + side, y + spaceY + dotW/2 + (spaceDotY * 2));
+  ctx.arc(x + spaceDotX + spaceX + dotW/2 + side, y + spaceY + dotW/2 + (spaceDotY * 2), dotW, 0, 2 * Math.PI);
+      ctx.moveTo(x + (spaceDotX * 2) + spaceX + dotW/2 + side, y + spaceY + dotW/2 + (spaceDotY * 2));
+  ctx.arc(x + (spaceDotX * 2) + spaceX + dotW/2 + side, y + spaceY + dotW/2 + (spaceDotY * 2), dotW, 0, 2 * Math.PI);
+      ctx.moveTo(x + (spaceDotX * 3) + spaceX + dotW/2 + side, y + spaceY + dotW/2 + (spaceDotY * 2));
+  ctx.arc(x + (spaceDotX * 3) + spaceX + dotW/2 + side, y + spaceY + dotW/2 + (spaceDotY * 2), dotW, 0, 2 * Math.PI);
+
+  ctx.fill();
+  //ctx.save();
+}
+
+
+
+function cls(){ctx.clearRect(0,0,c.width,c.height)}
